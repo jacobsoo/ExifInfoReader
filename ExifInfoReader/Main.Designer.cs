daa638a8f34beabc2670e3bec26a34bf129c3329
Jacob Soo <jacobsoo@users.noreply.github.com>
2014-01-12 11:12:55 +0800

    Initial commit

---
+++ ExifInfoReader/Main.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace ExifInfoReader
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.listExif = new System.Windows.Forms.ListView();
+            this.columnId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnField = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnDesc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.picPreview = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tssLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(712, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(344, 404);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // listExif
+            // 
+            this.listExif.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnId,
+            this.columnField,
+            this.columnDesc,
+            this.columnValue});
+            this.listExif.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listExif.FullRowSelect = true;
+            this.listExif.Location = new System.Drawing.Point(287, 3);
+            this.listExif.Name = "listExif";
+            this.listExif.Size = new System.Drawing.Size(419, 404);
+            this.listExif.TabIndex = 1;
+            this.listExif.UseCompatibleStateImageBehavior = false;
+            this.listExif.View = System.Windows.Forms.View.Details;
+            // 
+            // columnId
+            // 
+            this.columnId.Text = "ID";
+            this.columnId.Width = 50;
+            // 
+            // columnField
+            // 
+            this.columnField.Text = "Exif Field Name";
+            this.columnField.Width = 110;
+            // 
+            // columnDesc
+            // 
+            this.columnDesc.Text = "Description";
+            this.columnDesc.Width = 300;
+            // 
+            // columnValue
+            // 
+            this.columnValue.Text = "Value";
+            this.columnValue.Width = 140;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(1083, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Open...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // picPreview
+            // 
+            this.picPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picPreview.Location = new System.Drawing.Point(3, 3);
+            this.picPreview.Name = "picPreview";
+            this.picPreview.Size = new System.Drawing.Size(278, 404);
+            this.picPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picPreview.TabIndex = 4;
+            this.picPreview.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.10025F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.89975F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 349F));
+            this.tableLayoutPanel1.Controls.Add(this.picPreview, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.webBrowser1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listExif, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(18, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1059, 410);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tssLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 442);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1173, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tssLabel
+            // 
+            this.tssLabel.Name = "tssLabel";
+            this.tssLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(1084, 42);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 464);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ExifInfo Reader";
+            ((System.ComponentModel.ISupportInitialize)(this.picPreview)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ListView listExif;
+        private System.Windows.Forms.ColumnHeader columnId;
+        private System.Windows.Forms.ColumnHeader columnField;
+        private System.Windows.Forms.ColumnHeader columnDesc;
+        private System.Windows.Forms.ColumnHeader columnValue;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.PictureBox picPreview;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tssLabel;
+        private System.Windows.Forms.Button btnExit;
+
+
+    }
+}
+
---
+++ ExifInfoReader/Main.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+
+
+namespace ExifInfoReader
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private ExifTagCollection _exif;
+
+        private void AddTagToList(ExifTag tag)
+        {
+            ListViewItem item = listExif.Items.Add(tag.Id.ToString());
+            item.SubItems.Add(tag.FieldName);
+            item.SubItems.Add(tag.Description);
+            item.SubItems.Add(tag.Value);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "JPEG Files (*.jpg)|*.jpg|All Files (*.*)|*.*";
+            
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                tssLabel.Text = ofd.FileName;
+                bool bRes = false;
+                picPreview.Image = Image.FromFile(ofd.FileName);
+                listExif.Items.Clear();
+                _exif = new ExifTagCollection(ofd.FileName);
+
+                foreach (ExifTag tag in _exif)
+                {
+                    if (tag.Id == 1)
+                        bRes = true;
+                    AddTagToList(tag);
+                }
+                if (bRes == true)
+                {
+                    ExifTag NSTag = _exif[1];
+                    ExifTag LatTag = _exif[2];
+                    ExifTag EWTag = _exif[3];
+                    ExifTag LongTag = _exif[4];
+                    string lat = LatTag.Value.ToString();
+                    string[] lat_split = lat.Split(' ');
+                    if (lat != "")
+                    {
+                        StringBuilder queryAddress = new StringBuilder();
+                        queryAddress.Append("http://maps.googleapis.com/maps/api/staticmap?center=");
+                        string latNum1 = Regex.Replace(lat_split[0], "[^.0-9]", "");
+                        string latNum2 = Regex.Replace(lat_split[1], "[^.0-9]", "");
+                        string latNum3 = Regex.Replace(lat_split[2], "[^.0-9]", "");
+                        double latVal1 = double.Parse(latNum1);
+                        double latVal2 = double.Parse(latNum2);
+                        double latVal3 = double.Parse(latNum3);
+                        double lat_value = latVal1 + (latVal2 / 60) + (latVal3 / 3600);
+                        if (NSTag.Value.ToString().Substring(0, 5) == "South")
+                        {
+                            queryAddress.Append("-");
+                        }
+                        queryAddress.Append(lat_value);
+
+                        string lon = LongTag.Value.ToString();
+                        string[] lon_split = lon.Split(' ');
+                        string lonNum1 = Regex.Replace(lon_split[0], "[^.0-9]", "");
+                        string lonNum2 = Regex.Replace(lon_split[1], "[^.0-9]", "");
+                        string lonNum3 = Regex.Replace(lon_split[2], "[^.0-9]", "");
+                        double lonVal1 = double.Parse(lonNum1);
+                        double lonVal2 = double.Parse(lonNum2);
+                        double lonVal3 = double.Parse(lonNum3);
+                        double lon_value = lonVal1 + (lonVal2 / 60) + (lonVal3 / 3600);
+                        queryAddress.Append(",");
+
+                        if (EWTag.Value.ToString().Substring(0, 4) == "west")
+                        {
+                            queryAddress.Append("-");
+                        }
+                        queryAddress.Append(lon_value);
+                        queryAddress.Append("&zoom=10&size=400x400&maptype=terrain&sensor=true&key=ABQIAAAAaHAby4XeLCIadFkAUW4vmRSkJGe9mG57rOapogjk9M-sm4TzXxR2I7bi2Qkj-opZe16CdmDs7_dNrQ");
+
+                        webBrowser1.Navigate(queryAddress.ToString());
+                    }
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
